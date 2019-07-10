67aa9e6af7cbc8757e6f55835487770813760b62
dinanrhyt <46159537+dinanrhyt@users.noreply.github.com>
2019-07-10 23:56:56 +0700

    Add files via upload
    mengubah background gambar menggunakan python.

---
+++ tugas2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 19:47:42 2019
+
+@author: Toshiba
+"""
+import cv2 as cv
+import numpy as np
+
+# Load the aerial image and convert to HSV colourspace
+image = cv.imread("original.png")
+image = np.array(image, dtype=np.uint8)
+hsv=cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+# Define lower and uppper limits of what we call "brown"
+brown_lo=np.array([10,0,0])
+brown_hi=np.array([20,255,255])
+
+# Mask image to only select browns
+mask=cv.inRange(hsv,brown_lo,brown_hi)
+
+# Change image to red where we found brown
+image[mask>0]=(0,0,255)
+
+cv.imwrite("result.png",image)
---
+++ tugas2bgb.py
@@ -0,0 +1,28 @@
+
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 19:47:42 2019
+
+@author: Toshiba
+"""
+import cv2 as cv
+import numpy as np
+
+# Load the aerial image and convert to HSV colourspace
+image = cv.imread("bgresult.png")
+image = np.array(image, dtype=np.uint8)
+hsv=cv.cvtColor(image,cv.COLOR_BGR2HSV)
+
+# Define lower and uppper limits of what we call "red"
+lower = np.array([0,100,100])
+upper = np.array([20,255,255])
+
+
+# Mask image to only select browns
+mask=cv.inRange(hsv,lower,upper)
+
+# Change image to purple where we found red
+image[mask>0]=(205,0,205)
+
+cv.imwrite("bgresult2.png",image)
